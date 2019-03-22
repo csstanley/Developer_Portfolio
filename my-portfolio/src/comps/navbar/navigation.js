263145128e5bf3eb4cc42590b46ab13ad8cf2eb2
Christopher <csstanley84@gmail.com>
2019-03-21 22:15:02 -0400

    icon design unfinished

--- my-portfolio/src/comps/navbar/navigation.js
+++ my-portfolio/src/comps/navbar/navigation.js
@@ -1,77 +1,44 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
 import React, { Component } from 'react';
 import './navigation.css';
 
-var Github = require ('./github.png');
-var Linkedin = require ('./linkedin.png');
+var Github = require('./github.png');
+var Linkedin = require('./linkedin.png');
 
 
 
 class Navigation extends Component {
     render() {
         return (
-            <Navbar expand="lg" variant="light">
-                {/* <Nav
-                    activeKey="/"
-                    onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-                > */}
-                <Nav>
-                    <Row>
-                    <Col><Button variant="outline-secondary"><Nav.Link variant="pills" href="/">Home</Nav.Link></Button></Col>
-                    <Col><Button variant="outline-secondary"><Nav.Link variant="pills" href="/port" eventKey="link-1">Portfolio</Nav.Link></Button></Col>
-                    <Col><Button variant="outline-secondary"><Nav.Link variant="pills" href="/blog" eventKey="link-2">Blog</Nav.Link></Button></Col>
-                    <Col><Button variant="outline-secondary"><Nav.Link variant="pills" href="/contact" eventKey="link-3">Contact</Nav.Link></Button></Col>
-                    
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-               
-                    
-                    
-                    <Col><Image src={Github} className="SocialMedia" rounded /></Col>
-                    <Col><Image src={Linkedin} className="SocialMedia" rounded /></Col>  
-                    </Row>
+            <Navbar fixed="top">
+                    <Nav defaultActiveKey="/">
+                        <Nav.Item>
+                            <Nav.Link href="/"><Button variant="secondary"><Badge>Home</Badge></Button></Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/port" eventKey="link-1"><Button variant="secondary"><Badge>Portfolio</Badge></Button></Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/blog" eventKey="link-2"><Button variant="secondary"><Badge>Blog</Badge></Button></Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/contact" eventKey="link-3"><Button variant="secondary"><Badge>Contact</Badge></Button></Nav.Link>
+                        </Nav.Item>
+                    </Nav>
 
-                </Nav>
+                    <Nav className="justify-content-end">
+                        <Nav.Item>
+                            <Nav.Link href="https://github.com/csstanley"><Image src={Github} className="SocialMedia" /></Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="https://www.linkedin.com/in/christophersstanley/"><Image src={Linkedin} className="SocialMedia" /></Nav.Link>
+                        </Nav.Item>
+                    </Nav>
             </Navbar>
         )
     }
