8db64029be601fb371508253e2e8727e4bd3d7e5
Christopher <csstanley84@gmail.com>
2019-03-21 22:47:46 -0400

    unfinished card placeholder

--- my-portfolio/src/pages/port/port.js
+++ my-portfolio/src/pages/port/port.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 // import the css file //
 import Navigation from "../../comps/navbar/navigation.js";
+import Cards from "../../comps/cards/cards.js";
 
 class Port extends Component {
     render() {
         return (
+            <container>
+            <row>
             <div><Navigation /></div>
+            </row>
+            <row>
+            <div><Cards /></div>
+            </row>
+            </container>
         )
     }
     
